4a39b0cc527d19857a19064408f422d1390e3283
victorlcs <victor.loh95@gmail.com>
2020-10-29 22:15:57 +0800

    test

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname+'/dist/my-ThreeD-animation'));
+app.use(express.static(__dirname+'/dist/cool-shopping-list'));
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/my-ThreeD-animation/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/cool-shopping-list/index.html'));
 });
 app.listen(process.env.PORT || 8080);
