474d60b454e254e6500d7a505209c2b3b58bbe05
victorlcs <victor.loh95@gmail.com>
2020-10-30 23:06:24 +0800

    Added dynamic photo with button click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { phoneColor, photoUrl } from './data/data';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cool-shopping-list';
+  phoneColor = phoneColor;
+  buttonClicked: string = 'Black';
+  photoUrl = photoUrl;
 }
---
+++ src/app/data/data.ts
@@ -0,0 +1,6 @@
+export const phoneColor = ['Black', 'Gold', 'Red', 'Green', 'White'];
+
+export const photoUrl = {
+  Black: '../assets/iphone12ProMax.png',
+  Gold: '../assets/Gold.png',
+};
